0f2ac76df83b66c7725f62985b44b5d3f0b681ca
Adam A. Zajac <strider1551@gmail.com>
2010-08-13 15:10:21 -0400

    Fixed incorrect name for manpage in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup (name='djvubind',
-      version='0.1.0',
+      version='0.1.1',
       description='Creates djvu files with positional ocr, metadata, and bookmarks.',
       author='Adam Zajac',
       author_email='strider1551@gmail.com',
       url='https://code.google.com/p/djvubind/',
       license='GPL-3',
       py_modules=['Djvubind/__init__', 'Djvubind/ocr', 'Djvubind/organizer', 'Djvubind/utils'],
-      data_files=[('share/man/man1', ['docs/djvubind.1']),
+      data_files=[('share/man/man1', ['docs/djvubind.1.gz']),
                   ('bin', ['djvubind'])]
 )
