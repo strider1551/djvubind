631728886b507c0ecb92ed26370aad7506d7d702
Adam A. Zajac <strider1551@gmail.com>
2010-08-13 14:59:03 -0400

    Documentation and final touches for first release.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup (name='djvubind',
-      version='0.0.1',
+      version='0.1.0',
       description='Creates djvu files with positional ocr, metadata, and bookmarks.',
       author='Adam Zajac',
       author_email='strider1551@gmail.com',
-      url='',
+      url='https://code.google.com/p/djvubind/',
       license='GPL-3',
-      py_modules=['Djvubind/__init__'],
-      #data_files=[('share/man/man1', ['docs/lib2mp3.1.gz']),
-      #            ('bin', ['lib2mp3'])]
+      py_modules=['Djvubind/__init__', 'Djvubind/ocr', 'Djvubind/organizer', 'Djvubind/utils'],
+      data_files=[('share/man/man1', ['docs/djvubind.1']),
+                  ('bin', ['djvubind'])]
 )
