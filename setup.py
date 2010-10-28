b4934baf498c6224ffe718cf831bd29922b7b2ff
Adam A. Zajac <strider1551@gmail.com>
2010-10-28 13:29:14 -0400

    Updated setup.py for new modules.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup (name='djvubind',
-      version='0.3.1',
+      version='1.0.0',
       description='Creates djvu files with positional ocr, metadata, and bookmarks.',
       author='Adam Zajac',
       author_email='strider1551@gmail.com',
       url='https://code.google.com/p/djvubind/',
       license='GPL-3',
-      py_modules=['djvubind/__init__', 'djvubind/ocr', 'djvubind/organizer', 'djvubind/utils'],
+      py_modules=['djvubind/__init__', 'djvubind/encode', 'djvubind/ocr', 'djvubind/organizer', 'djvubind/utils'],
       data_files=[('bin', ['bin/djvubind']),
                   ('etc/djvubind', ['docs/config']),
                   ('share/man/man1', ['docs/djvubind.1.gz'])]
