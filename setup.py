b3224da15822651b3a82f8ee2b780ffa40fdeb3a
Adam A. Zajac <strider1551@gmail.com>
2010-11-01 14:05:26 -0400

    Config file installed to ~/.config/djvubind/config on first run (linux only)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup (name='djvubind',
-      version='1.0.1',
+      version='1.0.2',
       description='Creates djvu files with positional ocr, metadata, and bookmarks.',
       author='Adam Zajac',
       author_email='strider1551@gmail.com',
